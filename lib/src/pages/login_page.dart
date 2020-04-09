4bee941eb2af12e4d8aaf361080a1da5bdcf2a31
Luis Rangel <luisrangelc@gmail.com>
2020-04-08 22:45:11 -0600

    Login box design and fields

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,15 +7,115 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
+          _loginForm(context),
         ],
       ),
+    );
+  }
+
+  Widget _loginForm(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+          Container(
+            width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3.0,
+                      offset: Offset(0.0, 5.0),
+                      spreadRadius: 3.0)
+                ]),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Login',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(
+                  height: 60.0,
+                ),
+                _crearEmail(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _crearPassword(),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _crearBoton()
+              ],
+            ),
+          ),
+          Text('Forgot your password?'),
+          SizedBox(
+            height: 100.0,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            icon: Icon(
+              Icons.alternate_email,
+              color: Colors.deepPurple,
+            ),
+            hintText: 'ejemplo@correo.com',
+            labelText: 'Email'),
+      ),
+    );
+  }
+
+  Widget _crearPassword() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+            icon: Icon(
+              Icons.lock_outline,
+              color: Colors.deepPurple,
+            ),
+            labelText: 'Password'),
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Sign In'),
+      ),
+      onPressed: () {},
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
     );
   }
 
   Widget _crearFondo(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final fondoMorado = Container(
-      height: size.height * 0.4,
+      height: size.height * 0.6,
       width: double.infinity,
       decoration: BoxDecoration(
           gradient: LinearGradient(colors: <Color>[
@@ -61,7 +161,7 @@ class LoginPage extends StatelessWidget {
           left: -20.0,
         ),
         Container(
-          padding: EdgeInsets.only(top:80.0),
+          padding: EdgeInsets.only(top: 80.0),
           child: Column(
             children: <Widget>[
               Icon(
@@ -69,7 +169,10 @@ class LoginPage extends StatelessWidget {
                 color: Colors.white,
                 size: 75.0,
               ),
-              SizedBox(height: 10.0,width: double.infinity,),
+              SizedBox(
+                height: 10.0,
+                width: double.infinity,
+              ),
               Text(
                 'RPApos',
                 style: TextStyle(color: Colors.white, fontSize: 25.0),
