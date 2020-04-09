fc6f59725cdedd5ada37dc352669e8c5cdd1b7a2
Luis Rangel <luisrangelc@gmail.com>
2020-04-08 21:49:32 -0600

    Login wallpaper design

---
+++ lib/src/pages/login_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          _crearFondo(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearFondo(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final fondoMorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0)
+      ])),
+    );
+
+    final circulo = Container(
+      height: 100.0,
+      width: 100.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100.0),
+          color: Color.fromRGBO(255, 255, 255, 0.05)),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondoMorado,
+        Positioned(
+          child: circulo,
+          top: 90.0,
+          left: 30.0,
+        ),
+        Positioned(
+          child: circulo,
+          top: -40.0,
+          right: -30.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: -50.0,
+          right: -10.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: 120.0,
+          right: 20.0,
+        ),
+        Positioned(
+          child: circulo,
+          bottom: -50.0,
+          left: -20.0,
+        ),
+        Container(
+          padding: EdgeInsets.only(top:80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(
+                Icons.restaurant,
+                color: Colors.white,
+                size: 75.0,
+              ),
+              SizedBox(height: 10.0,width: double.infinity,),
+              Text(
+                'RPApos',
+                style: TextStyle(color: Colors.white, fontSize: 25.0),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
